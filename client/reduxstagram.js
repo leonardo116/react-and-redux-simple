ca128318f81bf68f739e072f8d3dc29825c1b124
richard8301 <richard8301@hotmail.com>
2019-06-15 18:39:10 +0800

    App Layout + Component Setup

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,12 @@
-// let's go!
+// ?let's go!
+import React from 'react';
+
+import { render } from 'react-dom';
+
+// Import css
+import css from './styles/style.styl';
+
+// Import components
+import Main from './components/Main';
+
+render(<Main />, document.getElementById('root'));
