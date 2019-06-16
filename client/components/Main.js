14573413261f144d97fa6ddf236093ac7aee6278
richard8301 <richard8301@hotmail.com>
2019-06-17 02:10:15 +0800

    Updating the State with Reducers

--- client/components/Main.js
+++ client/components/Main.js
@@ -4,12 +4,12 @@ import { Link } from 'react-router';
 const Main = React.createClass({
     render() {
         return (
-            < div >
+            <div>
                 <h1>
                     <Link to="/">Reduxstagram</Link>
                 </h1>
                 {React.cloneElement(this.props.children, this.props)}
-            </div >
+            </div>
         )
     }
 });
