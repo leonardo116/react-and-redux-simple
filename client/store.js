a6a0a620243c32fe3aa8f24d8785dc73565036a4
richard8301 <richard8301@hotmail.com>
2019-06-17 04:36:00 +0800

    Redux Dev Tools

--- client/store.js
+++ client/store.js
@@ -1,4 +1,4 @@
-import { createStore, compse } from 'redux';
+import { createStore, compose } from 'redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { browserHistory } from 'react-router';
 
@@ -14,6 +14,10 @@ const defaultState = {
     posts,
     comments
 };
+
+const enhancers = compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+);
 
 const store = createStore(rootReducer, defaultState);
 
