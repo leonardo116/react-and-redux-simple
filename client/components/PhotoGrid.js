dbe6ba94706c84fbc4447a1cfb5af42330dd8182
richard8301 <richard8301@hotmail.com>
2019-06-17 07:43:05 +0800

    photo-grid bug fixed

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -5,9 +5,7 @@ const PhotoGrid = React.createClass({
     render() {
         return (
             <div className="photo-grid">
-                <pre>
-                    {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
-                </pre>
+                {this.props.posts.map((post, i) => <Photo {...this.props} key={i} i={i} post={post} />)}
             </div>
         )
     }
